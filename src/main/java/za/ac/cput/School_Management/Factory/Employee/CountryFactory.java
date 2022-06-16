0bd7b6a0852a79c7bc35d6b22f73b472d6b4687c
KegoLeshope <leshopek48@gmail.com>
2022-06-16 11:32:54 +0200

    Kegomoditswe 1st commit

---
+++ src/main/java/za/ac/cput/School_Management/Factory/Employee/CountryFactory.java
@@ -0,0 +1,16 @@
+package za.ac.cput.School_Management.Factory.Employee;
+
+import za.ac.cput.School_Management.Domain.Employee.Country;
+
+public class CountryFactory {
+    public static Country build(String countryId, String countryName){
+        if (countryId == null || countryId.isEmpty())
+            throw new IllegalArgumentException("CountryId is required");
+
+        if (countryName == null || countryName.isEmpty())
+            throw new IllegalArgumentException("CountryName is required");
+
+        return new Country.Builder().countryId(countryId).countryName(countryName).build();
+
+    }
+}
---
+++ src/test/java/za/ac/cput/School_Management/Factory/Employee/CountryFactoryTest.java
@@ -0,0 +1,24 @@
+package za.ac.cput.School_Management.Factory.Employee;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.School_Management.Domain.Employee.Country;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryFactoryTest {
+
+    @Test
+    void buildWithSuccess() {
+        Country country = CountryFactory.build("ZA","South Africa");
+        System.out.println(country);
+        assertNotNull(country);
+    }
+
+    @Test
+    void buildWithError(){
+        Exception exception = assertThrows(IllegalArgumentException.class,() ->
+                CountryFactory.build(null,"South Africa"));
+        System.out.println(exception.getMessage());
+        assertTrue(exception.getMessage().contains("countryId"));
+    }
+}
