9b53306302a936a99e5ee0db69190cbf2bfcfdfb
Marvinhope24 <marvinhope070@gmail.com>
2022-06-16 15:50:59 +0200

    Employee Repository Completed

--- src/main/java/za/ac/cput/School_Management/Repository/Employee/Interface/IEmployeeRepository.java
+++ src/main/java/za/ac/cput/School_Management/Repository/Employee/Interface/IEmployeeRepository.java
@@ -1,4 +1,9 @@
 package za.ac.cput.School_Management.Repository.Employee.Interface;
 
-public interface IEmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import za.ac.cput.School_Management.Domain.Employee.Employee;
+
+@Repository
+public interface IEmployeeRepository extends JpaRepository <Employee, String> {
 }
