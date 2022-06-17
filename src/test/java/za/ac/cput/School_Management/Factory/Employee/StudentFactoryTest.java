a6c873e2acb56669f0dab309314790578b0c02a1
Tiffany <tiffanykiwiets@gmail.com>
2022-06-17 15:19:34 +0200

    Student factory and Repository completed.

---
+++ src/test/java/za/ac/cput/School_Management/Factory/Employee/StudentFactoryTest.java
@@ -0,0 +1,30 @@
+/*
+Tiffany Kiwiets - 219322732
+StudentFactoryTest.java
+16 June 2022
+ */
+
+package za.ac.cput.School_Management.Factory.Employee;
+
+import org.junit.jupiter.api.Test;
+import za.ac.cput.School_Management.Domain.Employee.Student;
+import za.ac.cput.School_Management.Domain.Employee.Name;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentFactoryTest {
+
+    @Test public void buildWithSuccess() {
+        Name name = NameFactory.build("Nicki", "Cardi", "Jhene");
+        Student student = StudentFactory.build("ten10", "nickicardi10@gmail.com", name);
+        System.out.println(student);
+        assertNotNull(student);
+    }
+
+    @Test public void buildWithError() {
+        Name name = NameFactory.build("Nicki", "Cardi", "Jhene");
+        Exception err = assertThrows(IllegalArgumentException.class, () -> StudentFactory.build(null, "nickicardi10@gmail.com", name));
+        String ExceptionMessage = err.getMessage();
+        System.out.println(err);
+        assertSame("Student ID is required.", ExceptionMessage);
+    }
+}
